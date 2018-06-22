c47dc49eb2f1820f0b30dd3d29f103be2cf0596a
Yoshikawa Taiki <yoshikawataiki@gmail.com>
2018-06-22 15:11:34 +0900

    【ADD】モデルの追加

--- models/user.go
+++ models/user.go
@@ -1,7 +1,16 @@
 package model
 
+import "time"
+
 // User Model
 type User struct {
-	ID   int64  `db:"id, primarykey"`
-	Name string `json:"name"`
+	Id             int64     `db:"id" json:"id"`
+	OrganizationID int64     `db:"organization_id" json:"organization_id"`
+	Name           string    `db:"name" json:"name"`
+	Email          string    `db:"email" json:"email"`
+	Password       string    `db:"password" json:"pasword"`
+	Country        string    `db:"country" json:"country"`
+	Administrator  int       `db:"administrator" json:"administrator"`
+	createdAt      time.Time `db:"created_at" json:"created_at"`
+	updatedAt      time.Time `db:"updated_at" json:"updated_at"`
 }
