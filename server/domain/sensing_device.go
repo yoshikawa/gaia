76599ed5efed72202158ebe0f64add4a40348347
YoshikawaTaiki <yoshikawataiki@gmail.com>
2018-11-16 19:56:11 +0900

    update models

---
+++ server/domain/sensing_device.go
@@ -0,0 +1,17 @@
+package domain
+
+import "time"
+
+// SensingDevice Model
+type SensingDevice struct {
+	ID                    int64     `json:"id" db:"id"`
+	ObservationPositionID int64     `json:"observation_position_id" db:"observation_position_id"`
+	VendorID              string    `json:"vendor_id" db:"vendor_id"`
+	DeviceType            string    `json:"device_type" db:"device_type"`
+	SerialNumber          string    `json:"serial_number" db:"serial_number"`
+	Name                  string    `json:"name" db:"name"`
+	TransmissionDistance  bool      `json:"transmission_distance" db:"transmission_distance"`
+	CreatedAt             time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt             time.Time `json:"updated_at" db:"updated_at"`
+	Deleted               time.Time `json:"deleted" db:"deleted"`
+}
---
+++ server/domain/sensing_device_status.go
@@ -0,0 +1,14 @@
+package domain
+
+import "time"
+
+// SensingDeviceStatus Model
+type SensingDeviceStatus struct {
+	ID              int64     `json:"id" db:"id"`
+	SensingDeviceID int64     `json:"sensing_device_id" db:"sensing_device_id"`
+	Battery         bool      `json:"battery" db:"battery"`
+	Status          bool      `json:"status" db:"status"`
+	Datetime        time.Time `json:"datetime" db:"datetime"`
+	CreatedAt       time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at" db:"updated_at"`
+}
