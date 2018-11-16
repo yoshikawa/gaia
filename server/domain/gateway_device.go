9c063562db08b643f53901efb5fdd314ca7e028c
YoshikawaTaiki <yoshikawataiki@gmail.com>
2018-11-16 19:12:47 +0900

    update

---
+++ server/domain/gateway_device.go
@@ -0,0 +1,18 @@
+package domain
+
+import "time"
+
+// GatewayDevice Model
+type GatewayDevice struct {
+	ID                   int64     `json:"id" db:"id"`
+	VendorID             int64     `json:"sensing_device_id" db:"vendor_id"`
+	DeviceType           string    `json:"device_type" db:"device_type"`
+	SerialNumber         string    `json:"serial_number" db:"serial_number"`
+	Point                float64   `json:"point" db:"point"`
+	Name                 string    `json:"name" db:"name"`
+	TransmissionDistance bool      `json:"transmission_distance" db:"transmission_distance"`
+	AuthenticationName   string    `json:"authentication_name" db:"authentication_name"`
+	Password             string    `json:"password" db:"password"`
+	CreatedAt            time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt            time.Time `json:"updated_at" db:"updated_at"`
+}
---
+++ server/domain/observable_property.go
@@ -0,0 +1,18 @@
+package domain
+
+import "time"
+
+// ObservableProperty Model
+type ObservableProperty struct {
+	ID                       int64     `json:"id" db:"id"`
+	ObservablePropertyNumber string    `json:"observable_property_number" db:"observable_property_number"`
+	System                   string    `json:"system" db:"system"`
+	Classification           string    `json:"classification" db:"classification"`
+	ClassificationEn         string    `json:"classification_en" db:"classification_en"`
+	ObservationProperty      string    `json:"observation_property" db:"observation_property"`
+	ObservationPropertyEn    string    `json:"observation_property_en" db:"observation_property_en"`
+	UnitOfMeasure            string    `json:"unit_of_measure" db:"unit_of_measure"`
+	DisplayUnit              string    `json:"display_unit" db:"display_unit"`
+	CreatedAt                time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt                time.Time `json:"updated_at" db:"updated_at"`
+}
