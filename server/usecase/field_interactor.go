b0e9a1f149800867a3ae661f9d0dc70568fdc36e
YoshikawaTaiki <yoshikawataiki@gmail.com>
2018-11-28 18:09:47 +0900

    update field api

--- server/usecase/field_interactor.go
+++ server/usecase/field_interactor.go
@@ -1,1 +1,30 @@
 package usecase
+
+import "github.com/Pluslab/fieldsensing/server/domain"
+
+// FieldInteractor model
+type FieldInteractor struct {
+	FieldRepository FieldRepository
+}
+
+// Add is to add field data
+func (interactor *FieldInteractor) Add(f domain.Field) (field domain.Field, err error) {
+	identifier, err := interactor.FieldRepository.Store(f)
+	if err != nil {
+		return
+	}
+	field, err = interactor.FieldRepository.FindByID(identifier)
+	return
+}
+
+// Fields is to find all fields
+func (interactor *FieldInteractor) Fields() (field domain.Fields, err error) {
+	field, err = interactor.FieldRepository.FindAll()
+	return
+}
+
+// FieldByID is to find field by id
+func (interactor *FieldInteractor) FieldByID(identifier int64) (field domain.Field, err error) {
+	field, err = interactor.FieldRepository.FindByID(identifier)
+	return
+}
