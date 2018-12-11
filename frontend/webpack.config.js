9d922c852ac29afdf8c999c833f2178099e92338
YoshikawaTaiki <yoshikawataiki@gmail.com>
2018-12-12 04:23:05 +0900

    update the front's development

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,46 +1,46 @@
-const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/main.tsx'),
+  entry: path.resolve(__dirname, "./src/main.tsx"),
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: './js/main.js'
+    path: path.resolve(__dirname, "./dist"),
+    filename: "./js/main.js"
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    }),
     new MiniCssExtractPlugin({
-      filename: './css/app.css'
+      filename: "./css/app.css"
     })
   ],
   resolve: {
     extensions: [
-      '.ts',  // for ts-loader
-      '.tsx',  // for ts-loader
-      '.js'
+      ".ts", // for ts-loader
+      ".tsx", // for ts-loader
+      ".js"
     ]
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader'
+        use: "ts-loader"
       },
       {
         test: /\.scss$/,
-        use: [
-	    MiniCssExtractPlugin.loader,
-            'css-loader',
-            'sass-loader'
-        ]
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
       },
       {
         test: /\.(jpg|png|gif)$/,
         use: {
-          loader: 'file-loader',
+          loader: "file-loader",
           options: {
-            name: './images/[name].[ext]',
-            outputPath: './',
-            publicPath: path => '.' + path
+            name: "./images/[name].[ext]",
+            outputPath: "./",
+            publicPath: path => "." + path
           }
         }
       },
@@ -54,5 +54,11 @@ module.exports = {
         ]
       }
     ]
+  },
+  devServer: {
+    contentBase: "./dist",
+    port: 8081,
+    inline: true,
+    host: "0.0.0.0"
   }
-}
+};
