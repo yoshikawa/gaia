66af3f8bd2b2327e48a5845d06d91733a9a84f3b
x16003xx <a87@pluslab.org>
2019-01-25 14:18:18 +0900

    commitする

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/main.tsx"),
+  entry: path.resolve(__dirname, "./src/js/main.tsx"),
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "./js/main.js"
@@ -11,6 +11,42 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'login.html',
+      template: "src/view/login.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'users/index.html',
+      template: "src/view/users/index.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'users/add.html',
+      template: "src/view/users/add.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'users/edit.html',
+      template: "src/view/users/edit.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'users/view.html',
+      template: "src/view/users/view.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'units/index.html',
+      template: "src/view/units/index.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'units/add.html',
+      template: "src/view/units/add.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'units/edit.html',
+      template: "src/view/units/edit.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'units/view.html',
+      template: "src/view/units/view.html"
     }),
     new MiniCssExtractPlugin({
       filename: "./css/app.css"
